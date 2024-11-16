2d617e8299e56d6685714ebaf0a9bea1f70a4b01
Do Quang Giap <167546581+giapdq04@users.noreply.github.com>
2024-11-16 14:18:56 +0700

    hoàn thành hàm thông báo 🚀🚀🚀

---
+++ notification/Notification.js
@@ -0,0 +1,42 @@
+var admin = require('./helper');
+const User = require('../models/userModel'); // Import model User
+
+const sendNotification = async (title, body, userList) => {
+    try {
+        // Lấy device_token của những người dùng trong danh sách
+        const users = await User.find({ _id: { $in: userList } }).select('device_token');
+        const tokens = users.map(user => user.device_token).filter(token => token); // Lọc ra các token hợp lệ
+        // console.log('userList: ', userList);
+
+
+        const result = await admin.messaging().sendEachForMulticast({
+            notification: {
+                title,
+                body,
+                imageUrl: 'https://play-lh.googleusercontent.com/DsyWoouXk7psjF7DCG6MJj_rX9RR9-liQskZXoKvcqQIu_ybUm4F5RntxWh1IZAVSLI'
+            },
+            android: {
+                notification: {
+                    sound: 'default'
+                }
+            },
+            apns: {
+                payload: {
+                    aps: {
+                        sound: 'default'
+                    }
+                }
+            },
+            tokens,
+        });
+        console.log('Successfully sent message:', result);
+        return {
+            notification: 'This is a notification'
+        };
+    } catch (error) {
+        console.error('Error sending message:', error);
+        throw new Error('Failed to send notification');
+    }
+};
+
+module.exports = { sendNotification };
